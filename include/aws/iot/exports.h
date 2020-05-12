860fd5736fed1e9c99c65283d98a1ec471e8a21c
David Oguns <ogudavid@amazon.com>
2020-05-11 17:26:25 -0700

    Initial add of CMake build project skeleton

---
+++ include/aws/iot/exports.h
@@ -0,0 +1,39 @@
+#ifndef AWS_IOT_EXPORTS_H
+#define AWS_IOT_EXPORTS_H
+
+/*
+ * Copyright 2010-2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+#if defined(USE_WINDOWS_DLL_SEMANTICS) || defined(WIN32)
+#    ifdef AWS_IOT_USE_IMPORT_EXPORT
+#        ifdef AWS_IOT_EXPORTS
+#            define AWS_IOT_API __declspec(dllexport)
+#        else
+#            define AWS_IOT_API __declspec(dllimport)
+#        endif /* AWS_IOT_EXPORTS */
+#    else
+#        define AWS_IOT_API
+#    endif /* USE_IMPORT_EXPORT */
+
+#else
+#    if ((__GNUC__ >= 4) || defined(__clang__)) && defined(AWS_IOT_USE_IMPORT_EXPORT) && defined(AWS_IOT_EXPORTS)
+#        define AWS_IOT_API __attribute__((visibility("default")))
+#    else
+#        define AWS_IOT_API
+#    endif /* __GNUC__ >= 4 || defined(__clang__) */
+
+#endif /* defined(USE_WINDOWS_DLL_SEMANTICS) || defined(WIN32) */
+
+#endif /* AWS_IOT_EXPORTS_H */
