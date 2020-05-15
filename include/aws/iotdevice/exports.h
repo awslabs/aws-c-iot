f46b4485636ce78b87ab4b529d6a8610e85203aa
David Oguns <ogudavid@amazon.com>
2020-05-15 14:13:08 -0700

    Added 'device' to API export. Updated builder version. Other minor stuff

--- include/aws/iotdevice/exports.h
+++ include/aws/iotdevice/exports.h
@@ -16,24 +16,24 @@
  * permissions and limitations under the License.
  */
 
-#if defined(USE_WINDOWS_DLL_SEMANTICS) || defined(__WIN32)
+#if defined(USE_WINDOWS_DLL_SEMANTICS) || defined(_WIN32)
 #    ifdef AWS_IOT_USE_IMPORT_EXPORT
 #        ifdef AWS_IOTDEVICE_EXPORTS
-#            define AWS_IOT_API __declspec(dllexport)
+#            define AWS_IOTDEVICE_API __declspec(dllexport)
 #        else
-#            define AWS_IOT_API __declspec(dllimport)
+#            define AWS_IOTDEVICE_API __declspec(dllimport)
 #        endif /* AWS_IOTDEVICE_EXPORTS */
 #    else
-#        define AWS_IOT_API
+#        define AWS_IOTDEVICE_API
 #    endif /* USE_IMPORT_EXPORT */
 
 #else
 #    if ((__GNUC__ >= 4) || defined(__clang__)) && defined(AWS_IOT_USE_IMPORT_EXPORT) && defined(AWS_IOTDEVICE_EXPORTS)
-#        define AWS_IOT_API __attribute__((visibility("default")))
+#        define AWS_IOTDEVICE_API __attribute__((visibility("default")))
 #    else
-#        define AWS_IOT_API
+#        define AWS_IOTDEVICE_API
 #    endif /* __GNUC__ >= 4 || defined(__clang__) */
 
-#endif /* defined(USE_WINDOWS_DLL_SEMANTICS) || defined(__WIN32) */
+#endif /* defined(USE_WINDOWS_DLL_SEMANTICS) || defined(_WIN32) */
 
 #endif /* AWS_IOT_EXPORTS_H */
