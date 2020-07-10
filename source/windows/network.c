319e98bf84f3c3ba87062b5321640743fdc30d0a
David Oguns <ogudavid@amazon.com>
2020-07-10 10:26:40 -0700

    Removing unused param warning->errors for Windows/Apple network.c

--- source/windows/network.c
+++ source/windows/network.c
@@ -12,9 +12,14 @@ struct aws_iotdevice_network_iface {
 
 void get_system_network_total(
     struct aws_iotdevice_metric_network_transfer *total,
-    struct aws_iotdevice_network_ifconfig *ifconfig) {}
+    struct aws_iotdevice_network_ifconfig *ifconfig) {
+    (void)total;
+    (void)ifconfig;
+}
 
 int get_network_config_and_transfer(struct aws_iotdevice_network_ifconfig *ifconfig, struct aws_allocator *allocator) {
+    (void)ifconfig;
+    (void)allocator;
     return AWS_ERROR_UNIMPLEMENTED;
 }
 
@@ -22,10 +27,17 @@ int get_net_connections(
     struct aws_array_list *net_conns,
     struct aws_allocator *allocator,
     const struct aws_iotdevice_network_ifconfig *ifconfig) {
+    (void)net_conns;
+    (void)ifconfig;
+    (void)allocator;
     return AWS_ERROR_UNIMPLEMENTED;
 }
 
 void get_network_total_delta(
     struct aws_iotdevice_metric_network_transfer *delta,
     struct aws_iotdevice_metric_network_transfer *prev_total,
-    struct aws_iotdevice_metric_network_transfer *curr_total) {}
+    struct aws_iotdevice_metric_network_transfer *curr_total) {
+    (void)delta;
+    (void)prev_total;
+    (void)curr_total;
+}
