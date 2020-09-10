587fe8a7e31c263af449c71dfcd71e4a143a4751
Francis Fu <fufranci@amazon.com>
2020-09-10 13:50:27 -0700

    address feedback except async

--- include/aws/iotdevice/secure_tunneling.h
+++ include/aws/iotdevice/secure_tunneling.h
@@ -4,29 +4,30 @@
 #include <aws/common/byte_buf.h>
 
 enum aws_secure_tunneling_local_proxy_mode {
-    AWS_SECURE_TUNNELING_SOURCE,
-    AWS_SECURE_TUNNELING_DESTINATION
+    AWS_SECURE_TUNNELING_SOURCE_MODE,
+    AWS_SECURE_TUNNELING_DESTINATION_MODE
 };
 
-typedef void(aws_secure_tunneling_data_msg_receive_fn)(int stream_id, const struct aws_byte_buf* data);
-typedef void(aws_secure_tunneling_control_msg_receive_fn)(int stream_id);
-typedef void(aws_secure_tunneling_close_fn)(int stream_id, int close_code);
+typedef void(aws_secure_tunneling_on_data_receive_fn)(int32_t stream_id, const struct aws_byte_buf* data);
+typedef void(aws_secure_tunneling_on_stream_start_fn)(int32_t stream_id);
+typedef void(aws_secure_tunneling_on_stream_reset_fn)(int32_t stream_id);
+typedef void(aws_secure_tunneling_on_close_fn)(int32_t stream_id, int32_t close_code);
 
 struct aws_secure_tunneling_connection_config {
-    const char *access_token;
+    struct aws_byte_cursor access_token;
     enum aws_secure_tunneling_local_proxy_mode mode;
-    const char *regional_endpoint;
+    struct aws_byte_cursor endpoint_host;
 
-    aws_secure_tunneling_data_msg_receive_fn *on_data_receive;
-    aws_secure_tunneling_control_msg_receive_fn *on_stream_start;
-    aws_secure_tunneling_control_msg_receive_fn *on_stream_reset;
-    aws_secure_tunneling_close_fn *on_close;
+    aws_secure_tunneling_on_data_receive_fn *on_data_receive;
+    aws_secure_tunneling_on_stream_start_fn *on_stream_start;
+    aws_secure_tunneling_on_stream_reset_fn *on_stream_reset;
+    aws_secure_tunneling_on_close_fn *on_close;
 };
 
-int aws_secure_tunneling_connect(const struct aws_secure_tunneling_connection_config *config);  // returns stream_id
-void aws_secure_tunneling_send_data(int stream_id, const struct aws_byte_buf* data);
-void aws_secure_tunneling_send_stream_start(int stream_id);
-void aws_secure_tunneling_send_stream_reset(int stream_id);
-void aws_secure_tunneling_close(int stream_id);
+int aws_secure_tunneling_connect(const struct aws_secure_tunneling_connection_config *config, int32_t *stream_id);
+int aws_secure_tunneling_send_data(int32_t stream_id, const struct aws_byte_buf* data);
+int aws_secure_tunneling_send_stream_start(int32_t stream_id);
+int aws_secure_tunneling_send_stream_reset(int32_t stream_id);
+int aws_secure_tunneling_close(int32_t stream_id);
 
-#endif // AWS_IOTDEVICE_SECURE_TUNNELING_H
+#endif /* AWS_IOTDEVICE_SECURE_TUNNELING_H */
