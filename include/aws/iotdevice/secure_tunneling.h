f4cba1b15c55949b3221ca7b833e6038b404969b
Francis Fu <fufranci@amazon.com>
2020-10-06 12:09:50 -0700

    Define aws_secure_tunnel struct and it's vtable. Use aws_secure_tunnel in APIs.

--- include/aws/iotdevice/secure_tunneling.h
+++ include/aws/iotdevice/secure_tunneling.h
@@ -8,11 +8,33 @@
 
 enum aws_secure_tunneling_local_proxy_mode { AWS_SECURE_TUNNELING_SOURCE_MODE, AWS_SECURE_TUNNELING_DESTINATION_MODE };
 
-typedef void(aws_secure_tunneling_on_connection_complete_fn)(int32_t stream_id);
-typedef void(aws_secure_tunneling_on_data_receive_fn)(int32_t stream_id, const struct aws_byte_buf *data);
-typedef void(aws_secure_tunneling_on_stream_start_fn)(int32_t stream_id);
-typedef void(aws_secure_tunneling_on_stream_reset_fn)(int32_t stream_id);
-typedef void(aws_secure_tunneling_on_close_fn)(int32_t stream_id, int32_t close_code);
+struct aws_secure_tunnel;
+
+/* APIs */
+typedef int (aws_secure_tunneling_send_data_fn)(struct aws_secure_tunnel *secure_tunnel, const struct aws_byte_buf* data);
+typedef int (aws_secure_tunneling_send_stream_start_fn)(struct aws_secure_tunnel *secure_tunnel);
+typedef int (aws_secure_tunneling_send_stream_reset_fn)(struct aws_secure_tunnel *secure_tunnel);
+typedef int (aws_secure_tunneling_close_fn)(struct aws_secure_tunnel *secure_tunnel);
+
+struct aws_secure_tunnel_vtable {
+    aws_secure_tunneling_send_data_fn *aws_secure_tunneling_send_data;
+    aws_secure_tunneling_send_stream_start_fn *aws_secure_tunneling_send_stream_start;
+    aws_secure_tunneling_send_stream_reset_fn *aws_secure_tunneling_send_stream_reset;
+    aws_secure_tunneling_close_fn *aws_secure_tunneling_close;
+};
+
+struct aws_secure_tunnel {
+    int32_t stream_id;
+    struct aws_websocket *websocket;
+    struct aws_secure_tunnel_vtable *vtable;
+};
+
+/* Callbacks */
+typedef void(aws_secure_tunneling_on_connection_complete_fn)(struct aws_secure_tunnel *secure_tunnel);
+typedef void(aws_secure_tunneling_on_data_receive_fn)(struct aws_secure_tunnel *secure_tunnel, const struct aws_byte_buf *data);
+typedef void(aws_secure_tunneling_on_stream_start_fn)(struct aws_secure_tunnel *secure_tunnel);
+typedef void(aws_secure_tunneling_on_stream_reset_fn)(struct aws_secure_tunnel *secure_tunnel);
+typedef void(aws_secure_tunneling_on_close_fn)(struct aws_secure_tunnel *secure_tunnel, int32_t close_code);
 
 struct aws_secure_tunneling_connection_config {
     struct aws_allocator *allocator;
@@ -32,14 +54,5 @@ struct aws_secure_tunneling_connection_config {
 
 AWS_SECURE_TUNNELING_API
 int aws_secure_tunneling_connect(const struct aws_secure_tunneling_connection_config *connection_config);
-
-AWS_SECURE_TUNNELING_API
-int aws_secure_tunneling_close(int32_t stream_id);
-
-/*
-int aws_secure_tunneling_send_data(int32_t stream_id, const struct aws_byte_buf* data);
-int aws_secure_tunneling_send_stream_start(int32_t stream_id);
-int aws_secure_tunneling_send_stream_reset(int32_t stream_id);
-*/
 
 #endif /* AWS_IOTDEVICE_SECURE_TUNNELING_H */
