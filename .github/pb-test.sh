79a9cfeef318998c4f44cb0b4ad2fa650a72acf8
lwwilkov <lwwilkov@amazon.com>
2020-10-06 12:53:52 -0400

    Change to pb-test

--- .github/pb-test.sh
+++ .github/pb-test.sh
@@ -6,16 +6,17 @@ mkdir build
 cd build
 cmake -DBUILD_DEPS=ON ../
 make
-var=0
+failed=0
+set -e
 for i in {1..6}
 do
   ./tests/tests_protobuf/aws-c-iot-st-pb-test $i
   if [ $? != 0 ]
   then
-    var=$?
+    failed=$?
   fi
 done
-if [ "$var" != 0 ]
+if [ "$failed" != 0 ]
 then
     exit 1
 fi
