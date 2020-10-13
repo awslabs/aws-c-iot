bb54311438c519a130fb9598e300325a1fbcd971
lwwilkov <lwwilkov@amazon.com>
2020-10-13 19:28:41 -0400

    small change

--- source/secure_tunneling.c
+++ source/secure_tunneling.c
@@ -192,7 +192,6 @@ static int s_secure_tunneling_send_data(struct aws_secure_tunnel *secure_tunnel,
     if (buffer.len > MAX_ST_PAYLOAD) {
         return AWS_OP_ERR;
     }
-
     struct aws_websocket_send_frame_options frame_options;
     s_init_websocket_send_frame_options(&frame_options, x);
     aws_websocket_send_frame(secure_tunnel->websocket, &frame_options);
@@ -208,7 +207,7 @@ static int s_secure_tunneling_send_stream_start(struct aws_secure_tunnel *secure
     message.streamId = secure_tunnel->stream_id;
     message.ignorable = 0;
     message.type = STREAM_START;
-    message.payload.len = 0;
+    message.payload = aws_byte_buf_from_c_str("");
 
     struct aws_byte_buf buffer;
     AWS_RETURN_ERROR_IF2(
@@ -228,7 +227,7 @@ static int s_secure_tunneling_send_stream_reset(struct aws_secure_tunnel *secure
     message.streamId = secure_tunnel->stream_id;
     message.ignorable = 0;
     message.type = STREAM_RESET;
-    message.payload.len = 0;
+    message.payload = aws_byte_buf_from_c_str("");
 
     struct aws_byte_buf buffer;
     AWS_RETURN_ERROR_IF2(
