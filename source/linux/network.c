1023bbae4be072111ab6677971376a37e3f77de1
TwistedTwigleg <ncbeard@amazon.com>
2022-05-03 10:23:48 -0400

    RHEL5 support (#70)
    Only uses if_link.h if the header exists, allowing support for platforms like RHEL5 that do not have if_link.h.
    
    Commit log:
    * Import if_link.h only if the header exists
    * Clang-format fix
    * Use cmake check include file that works in C
    * Adjust how CMAKE definitions are added to support older versions of CMAKE
    * Use feature test in aws-c-common to detect if_link.h
    * Remove unused defines

--- source/linux/network.c
+++ source/linux/network.c
@@ -5,13 +5,18 @@
 
 #include <aws/iotdevice/private/network.h>
 
+#include <aws/common/config.h>
 #include <aws/common/hash_table.h>
 #include <aws/common/string.h>
 
 #include <arpa/inet.h>
 #include <errno.h>
 #include <ifaddrs.h>
-#include <linux/if_link.h>
+
+#if defined(AWS_HAVE_LINUX_IF_LINK_H)
+#    include <linux/if_link.h>
+#endif
+
 #include <net/if.h>
 #include <netinet/in.h>
 #include <stdio.h>
@@ -389,6 +394,8 @@ int get_network_config_and_transfer(struct aws_iotdevice_network_ifconfig *ifcon
         strncpy(iface->iface_name, ifr.ifr_name, IFACE_NAME_SIZE);
         iface->iface_name[IFACE_NAME_SIZE - 1] = 0;
 
+#if defined(AWS_HAVE_LINUX_IF_LINK_H)
+        // Only populate if_link.h exists
         if (address->ifa_data) {
             struct rtnl_link_stats *stats = address->ifa_data;
             iface->metrics.bytes_in = stats->rx_bytes;
@@ -396,6 +403,7 @@ int get_network_config_and_transfer(struct aws_iotdevice_network_ifconfig *ifcon
             iface->metrics.packets_in = stats->rx_packets;
             iface->metrics.packets_out = stats->tx_packets;
         }
+#endif
 
         if ((result = aws_hash_table_put(&ifconfig->iface_name_to_info, iface->ipv4_addr_str, iface, NULL))) {
             AWS_LOGF_ERROR(
